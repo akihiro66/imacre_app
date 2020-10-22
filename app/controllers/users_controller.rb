305477bc09f9caec84c1b3d7b6f7ae31b1113125
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-10-22 10:22:14 +0900

    作品一覧をユーザーのプロフィールページに表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @productions = @user.productions.paginate(page: params[:page], per_page: 5)
   end
 
   def index
