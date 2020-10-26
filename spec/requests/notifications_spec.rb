0bfcfcfc3894d8da3e7dd6464d01c14715f8bfc2
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-10-27 01:05:38 +0900

    通知機能のテストを追加

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 RSpec.describe "通知機能", type: :request do
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:production) { create(:production, user: user) }
+  let!(:other_production) { create(:production, user: other_user) }
+
 
   context "通知一覧ページの表示" do
     context "ログインしているユーザーの場合" do
@@ -20,6 +24,40 @@ RSpec.describe "通知機能", type: :request do
         get notifications_path
         expect(response).to have_http_status "302"
         expect(response).to redirect_to login_path
+      end
+    end
+  end
+
+  context "通知処理" do
+    before do
+      login_for_request(user)
+    end
+
+    context "自分以外のユーザーの作品に対して" do
+      it "お気に入り登録によって通知が作成されること" do
+        post "/favorites/#{other_production.id}/create"
+        expect(user.reload.notification).to be_falsey
+        expect(other_user.reload.notification).to be_truthy
+      end
+
+      it "コメントによって通知が作成されること" do
+        post comments_path, params: { production_id: other_production.id,
+                                      comment: { content: "最高です！" } }
+        expect(user.reload.notification).to be_falsey
+        expect(other_user.reload.notification).to be_truthy
+      end
+    end
+
+    context "自分の作品に対して" do
+      it "お気に入り登録によって通知が作成されないこと" do
+        post "/favorites/#{production.id}/create"
+        expect(user.reload.notification).to be_falsey
+      end
+
+      it "コメントによって通知が作成されないこと" do
+        post comments_path, params: { production_id: production.id,
+                                      comment: { content: "最高です！" } }
+        expect(user.reload.notification).to be_falsey
       end
     end
   end
