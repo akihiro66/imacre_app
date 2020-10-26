36c87109e5f590460afb5f23cb4782656c87e0eb
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-10-26 18:26:19 +0900

    お気に入り登録のindexアクションを実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
   before_action :logged_in_user
 
+  def index
+    @favorites = current_user.favorites
+  end
+
   def create
     @production = Production.find(params[:production_id])
     @user = @production.user
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :production
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :production_id, presence: true
 end
