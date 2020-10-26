ffca389ff6546f7164466297b8a085346eb328ca
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-10-26 17:53:40 +0900

    お気に入りのcreate/destroyアクション実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,21 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @production = Production.find(params[:production_id])
+    @user = @production.user
+    current_user.favorite(@production)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    @production = Production.find(params[:production_id])
+    current_user.favorites.find_by(production_id: @production.id).destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
