9b9e185b2c793f34a98680c1a7e73132f55155df
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-10-26 22:50:26 +0900

    コメントのcreateアクションを実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,15 @@ class CommentsController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @production = Production.find(params[:production_id])
+    @user = @production.user
+    @comment = @production.comments.build(user_id: current_user.id, content: params[:comment][:content])
+    if !@production.nil? && @comment.save
+      flash[:success] = "コメントを追加しました！"
+    else
+      flash[:danger] = "空のコメントは投稿できません。"
+    end
+    redirect_to request.referrer || root_url
   end
 
   def destroy
