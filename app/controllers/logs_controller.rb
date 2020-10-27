5f6c8d94f9c674d3f0718e79c6e50caa96eced47
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-10-27 20:06:27 +0900

    製作記録をトップページ、マイページ、製作予定リストページからも出来るように実装

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,11 +1,14 @@
 class LogsController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user, only: :create
 
   def create
     @production = Production.find(params[:production_id])
     @log = @production.logs.build(content: params[:log][:content])
     @log.save
     flash[:success] = "制作記録を追加しました！"
+    # 製作予定リストページから製作記録が作成された場合、その作品をリストから削除
+    List.find(params[:list_id]).destroy unless params[:list_id].nil?
     redirect_to production_path(@production)
   end
 
@@ -18,4 +21,12 @@ class LogsController < ApplicationController
     end
     redirect_to production_url(@production)
   end
+
+  private
+
+    def correct_user
+      # 現在のユーザーが対象の作品を保有しているかどうか確認
+      production = current_user.productions.find_by(id: params[:production_id])
+      redirect_to root_url if production.nil?
+    end
 end
