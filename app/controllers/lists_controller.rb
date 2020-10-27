65e454b5d70a0409547332f3003cae8a58fe20ac
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-10-27 10:48:11 +0900

    リストのcreate/destroyアクションを実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,8 +5,22 @@ class ListsController < ApplicationController
   end
 
   def create
+    @production = Production.find(params[:production_id])
+    @user = @production.user
+    current_user.list(@production)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    list = List.find(params[:list_id])
+    @production = list.production
+    list.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
